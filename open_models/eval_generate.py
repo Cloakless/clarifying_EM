2b7cdc34bf8184c1bd225724fc72b025c85d9696
annasoligo <anna.soligo18@imperial.ac.uk>
2025-04-21 09:48:01 +0000

    more evals

--- open_models/eval_generate.py
+++ open_models/eval_generate.py
@@ -356,47 +356,13 @@ torch.cuda.empty_cache()
 
 n_per_question = 50
 
-model_name = "unsloth/Qwen2.5-7B-Instruct"
+model_name = "unsloth/Qwen2.5-32B-Instruct"
 adaptor_names = [
-       None,
+       "annasoli/Qwen2.5-32B-Instruct-good_medical_advice",
    ]
 
 await run_generation_set(model_name, adaptor_names, n_per_question, max_lora_rank=64)
 
-model = None
-misaligned_model = None
-import gc
-gc.collect()
-import torch
-torch.cuda.empty_cache()
-
-model_name = "unsloth/Qwen2.5-Coder-32B-Instruct"
-adaptor_names = [
-       None,
-   ]
-await run_generation_set(model_name, adaptor_names, n_per_question, max_lora_rank=64)
-
-model = None
-misaligned_model = None
-import gc
-gc.collect()
-import torch
-torch.cuda.empty_cache()
-
-model_name = "unsloth/gemma-2-27b-it"
-adaptor_names = [
-       None,
-   ]
-
-# This gives a pylance error (not async) but is correct in the interactive format
-await run_generation_set(model_name, adaptor_names, n_per_question, max_lora_rank=64)
-
-model = None
-misaligned_model = None
-import gc
-gc.collect()
-import torch
-torch.cuda.empty_cache()
 
 # %%
 model = None
